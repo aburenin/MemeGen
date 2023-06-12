ad44e66389096ce9f61d89dfff365cf18c3906b4
aburenin <125705453+aburenin@users.noreply.github.com>
2023-06-12 14:18:15 +0200

    Create mem_gen_request.py
    create mem_generator

---
+++ mem_gen_request.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+def list_meme():
+    url = 'http://apimeme.com'
+    response = requests.get(url=url)
+    # Превращаем HTML-код в объект BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    options = soup.find_all('option')
+    mems = [option.get('value') for option in options]
+    return mems
+
+def generate_meme(top_text, bottom_text, meme):
+
+    url = f'http://apimeme.com/meme?meme={meme}&top={top_text}&bottom={bottom_text}'
+
+    response = requests.get(url=url)
+
+    if response.status_code == 200:
+        with open('meme.png', 'wb') as img_file:
+            img_file.write(response.content)
+    else:
+        print(f"Request failed with status code {response.status_code}")
+
+
+def main():
+    #generate_meme('Top Test','Test_2','10-Guy')
+    list_meme()
+
+if __name__ == '__main__':
+    main()
